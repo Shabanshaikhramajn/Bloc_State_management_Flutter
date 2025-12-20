925558f94038d3923f1c1a0a26a5d488c0ac2dfa
shaban <shabanshaikh72345@gmail.com>
2025-12-20 10:21:40 +0530

    bloc bind data

--- lib/bloc/bottom_bloc.dart
+++ lib/bloc/bottom_bloc.dart
@@ -3,5 +3,4 @@ import 'package:state_management/bloc/app_bloc.dart';
 class BottomBloc extends AppBloc {
   BottomBloc({ Duration? waitBeforeLoding, required Iterable<String>urls}): super
       (waitBeforeDuration: waitBeforeLoding, urls: urls);
-
 }
